a55c81dfd9a2cd6fe8fd166754a40bdc3fc2c1fc
vae <Ntalley331@gmail.com>
2022-11-27 21:55:24 -0700

    first commit

---
+++ js/config.js
@@ -0,0 +1,15 @@
+const config = {
+    apiKey: "AIzaSyBPKgDm0GdZkI9dXJXGz4xqr9PbxPZUqIU",
+    authDomain: "chap-app-d569e.firebaseapp.com",
+    databaseURL: "https://chap-app-d569e-default-rtdb.firebaseio.com",
+    projectId: "chap-app-d569e",
+    storageBucket: "chap-app-d569e.appspot.com",
+    messagingSenderId: "412037649684",
+    appId: "1:412037649684:web:aaae9c209e7f44abbc1e01",
+    measurementId: "G-RDTQV9WQS7",
+  CometChatAppId: "226548dbbbca9691",
+  CometChatRegion: us,
+  CometChatAuthKey: "a395baed183e7403d36abe734d5cc6c090cd5033",
+  CometChatAPIKey: "a395baed183e7403d36abe734d5cc6c090cd5033",
+  CometChatWidgetId: "6610a8bc-d756-49fa-8419-52c19a668e3c"
+};
